2eca2f7b6643982f53f89baea829752a002a67f6
Victor <lmslovecs@gmail.com>
2018-05-22 23:25:41 +0800

    refactor(module: wink-ide-right-panel): move code to wink-ide-top-menu

--- src/app/wink-ide-right-panel/wink-ide-right-panel.component.ts
+++ src/app/wink-ide-right-panel/wink-ide-right-panel.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-wink-ide-right-panel',
   templateUrl: './wink-ide-right-panel.component.html',
-  styleUrls: ['./wink-ide-right-panel.component.css']
+  styleUrls: ['./wink-ide-right-panel.component.less']
 })
 export class WinkIdeRightPanelComponent implements OnInit {
-
+  //
+  // // TODO: is this ok to use Array? Or better alternative?
+  // topMenus: Array = [
+  //   {'anticon-file-add' : '新建'},
+  //   {'anticon-copy' : '另存为'},
+  //   {'anticon-save' : '保存'},
+  //   {'anticon-swap-left' : '撤销'},
+  //   {'anticon-swap-right' : '重做'},
+  //   {'anticon-play-circle-o' : '调试'},
+  //   {'anticon-safety' : '语法检查'},
+  //   {'anticon-cloud-upload-o' : '上线'},
+  //   {'anticon-play-circle-o' : '运维'}
+  //
+  // ];
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  triggerSave() {
+    console.log('heh');
+  }
 }
