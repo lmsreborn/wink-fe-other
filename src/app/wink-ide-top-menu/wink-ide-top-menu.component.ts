1758787f026ecb9c0e1b41db32ea50ae62982b60
Victor <lmslovecs@gmail.com>
2018-05-22 23:28:25 +0800

    feat(module:wink-ide-top-menu): add new menu

--- src/app/wink-ide-top-menu/wink-ide-top-menu.component.ts
+++ src/app/wink-ide-top-menu/wink-ide-top-menu.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-wink-ide-top-menu',
   templateUrl: './wink-ide-top-menu.component.html',
-  styleUrls: ['./wink-ide-top-menu.component.css']
+  styleUrls: ['./wink-ide-top-menu.component.less']
 })
 export class WinkIdeTopMenuComponent implements OnInit {
 
