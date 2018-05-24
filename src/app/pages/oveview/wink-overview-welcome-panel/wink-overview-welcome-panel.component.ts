76d7808c9fae72fd63739c92ba75db4b167f1fe8
Victor <lmslovecs@gmail.com>
2018-05-24 22:53:03 +0800

    feat(module: wink-fe-overview): add welcome info and resource info

--- src/app/pages/oveview/wink-overview-welcome-panel/wink-overview-welcome-panel.component.ts
+++ src/app/pages/oveview/wink-overview-welcome-panel/wink-overview-welcome-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-wink-overview-welcome-panel',
@@ -7,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WinkOverviewWelcomePanelComponent implements OnInit {
 
-  constructor() { }
+
+  imageURL = 'assets/images/user.png';
+
+  userName = 'lms';
+
+  projectName = '测试项目';
+
+  projectDescription = '当前项目为: ';
+
+  usedCUNum = 1;
+  purchasedCUNum = 10;
+
+  usage = (this.usedCUNum * 100 * 1.00 / this.purchasedCUNum).toFixed(2);
+
+  leftUsageDays = 10;
+
+  resourceNotification = '剩余可用时间不足' + this.leftUsageDays.toString() + '天，请尽快续费';
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
