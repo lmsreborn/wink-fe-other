2cf4dc9ef8bcb396858a59a48cbb79f209e03d07
Victor <lmslovecs@gmail.com>
2018-05-29 14:59:41 +0800

    fix(module: wink): add new content in wink-resize-opbar

--- src/app/wink-resize-opbar/wink-resize-opbar.component.ts
+++ src/app/wink-resize-opbar/wink-resize-opbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-wink-resize-opbar',
   templateUrl: './wink-resize-opbar.component.html',
-  styleUrls: ['./wink-resize-opbar.component.css']
+  styleUrls: ['./wink-resize-opbar.component.less']
 })
 export class WinkResizeOpbarComponent implements OnInit {
 
