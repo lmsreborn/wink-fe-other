82ce9843834815f461b65c9197a8b4f5535ee9e9
Victor <lmslovecs@gmail.com>
2018-05-30 21:57:41 +0800

    feat(module:wink-operate): support Chinese in datapicker

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import zh from '@angular/common/locales/zh';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {registerLocaleData} from '@angular/common';
+
 
 if (environment.production) {
   enableProdMode();
 }
+
+// TODO: 位置是否正确
+registerLocaleData(zh);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
