802a5f24aaa2e6018468d512041d10e36ee26a31
Victor <lmslovecs@gmail.com>
2018-05-30 10:04:40 +0800

    feat(module:wink-operate): add vertexListTable in the dashboard

--- src/app/pages/operate/wink-operate-instance-dashboard/wink-operate-instance-dashboard.component.ts
+++ src/app/pages/operate/wink-operate-instance-dashboard/wink-operate-instance-dashboard.component.ts
@@ -7,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WinkOperateInstanceDashboardComponent implements OnInit {
 
+  vertexListData = [
+    {
+      ID: 0,
+      Name: 'Source: DataHubTableSource-inflame-test1 -> SourceParser:DefaultSourceCollector(DataHubTableSource-inflame-test1) -> from: (f1, f2) -> where: (<(f1, 50)), select: (f1, f2) -> to: Tuple2 -> Sink: TupleOutputFormatAdapterSink:com.al...',
+      Status: 'RUNNING',
+      InQ_max: '0(0.00%)',
+      OutQ_max: '0(0.00%)',
+      RecCnt_sum: '0(0.00%)',
+      SendCnt_sum: '0(0.00%)',
+      TPS_sum: '0(0.00%)',
+      Delay_max: '0(0.00%)',
+      StartTime: '0(0.00%)',
+      Duration: '0(0.00%)',
+      TaskNum: 3
+    },
+    {
+      ID: 1,
+      Name: 'Source: DataHubTableSource-inflame-test1 -> SourceParser:DefaultSourceCollector(DataHubTableSource-inflame-test1) -> from: (f1, f2) -> where: (<(f1, 50)), select: (f1, f2) -> to: Tuple2 -> Sink: TupleOutputFormatAdapterSink:com.al...',
+      Status: 'RUNNING',
+      InQ_max: '0(0.00%)',
+      OutQ_max: '0(0.00%)',
+      RecCnt_sum: '0(0.00%)',
+      SendCnt_sum: '0(0.00%)',
+      TPS_sum: '0(0.00%)',
+      Delay_max: '0(0.00%)',
+      StartTime: '0(0.00%)',
+      Duration: '0(0.00%)',
+      TaskNum: 3
+    },
+    {
+      ID: 0,
+      Name: 'Source: DataHubTableSource-inflame-test1 -> SourceParser:DefaultSourceCollector(DataHubTableSource-inflame-test1) -> from: (f1, f2) -> where: (<(f1, 50)), select: (f1, f2) -> to: Tuple2 -> Sink: TupleOutputFormatAdapterSink:com.al...',
+      Status: 'RUNNING',
+      InQ_max: '0(0.00%)',
+      OutQ_max: '0(0.00%)',
+      RecCnt_sum: '0(0.00%)',
+      SendCnt_sum: '0(0.00%)',
+      TPS_sum: '0(0.00%)',
+      Delay_max: '0(0.00%)',
+      StartTime: '0(0.00%)',
+      Duration: '0(0.00%)',
+      TaskNum: 3
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
