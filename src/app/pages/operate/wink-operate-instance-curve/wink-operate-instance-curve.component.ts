c6eb981052e39f2a96f8842191fe39f596a50c66
Victor <lmslovecs@gmail.com>
2018-06-03 13:09:41 +0800

    feat(module: wink-operate-instance-curve): add new charts in the component

--- src/app/pages/operate/wink-operate-instance-curve/wink-operate-instance-curve.component.ts
+++ src/app/pages/operate/wink-operate-instance-curve/wink-operate-instance-curve.component.ts
@@ -61,83 +61,173 @@ export class WinkOperateInstanceCurveComponent implements OnInit {
           info: '衡量整个Job每次做checkpoint时消耗的时间'
         },
         {
-          title: '最小延时',
-          info: '衡量数据源的延迟情况，一段时间内的最小值'
+          title: 'CheckpointSize',
+          info: '衡量整个Job每次做checkpoint时占用的存储空间大小'
         },
         {
-          title: '最大时延',
-          info: '衡量数据源的延迟情况，一段时间内的最大值'
+          title: 'checkpointAlignmentTime',
+          info: '衡量一段时间内每个Vertex做checkpoint对齐的时间（最大值）'
         },
         {
-          title: '平均时延',
-          info: '衡量数据源的延迟情况，一段时间内的平均值'
+          title: 'checkpointCount',
+          info: '衡量整个Job的checkpoint个数'
         },
         {
-          title: '各Source的TPS数据输入',
-          info: '衡量数据源流入的流量，Block粒度（数据源为SLS或TT）'
+          title: 'Get',
+          info: '衡量一段时间内每个subtask操作rocksdb的时间（最大值）'
         },
         {
-          title: '各Sink的数据输出',
-          info: '衡量向目的数据端流出的流量，Record粒度'
+          title: 'Put',
+          info: '衡量一段时间内每个subtask操作rocksdb的时间（最大值）'
         },
         {
-          title: '各Source的RPS数据输入',
-          info: '衡量数据源流入的流量，Record粒度（数据源为DataHub时）'
+          title: 'Seek',
+          info: '衡量一段时间内每个subtask操作rocksdb的时间（最大值）'
         },
         {
-          title: '各Source的数据流量输入',
-          info: '衡量数据源流入的流量，Byte粒度'
+          title: 'State Size',
+          info: '衡量整个Job内部state存储的大小，如果增长过快则有问题'
         },
         {
-          title: '各Source的脏数据',
-          info: '各Source的脏数据'
+          title: 'GMS GC Time',
+          info: '衡量运行Job的底层container在GC上耗费的时间'
         },
         {
-          title: '各Source的脏数据',
-          info: '各Source的脏数据'
+          title: 'GMS GC Rate',
+          info: '衡量运行Job的底层container进行GC的频率'
         },
         {
-          title: '各Source的脏数据',
-          info: '各Source的脏数据'
-        },
-        {
-          title: '各Source的脏数据',
-          info: '各Source的脏数据'
+          title: 'WaterMark Delay',
+          info: 'WaterMark 距离系统时间的差值'
         }
       ]
     };
 
   delayPanel =
     {
-      header: 'Overview',
-      isActive: true,
+      header: 'Delay',
+      isActive: false,
       charts: [
         {
-          title: 'FailOver rate',
-          info: '衡量当前Job发生failover的频率'
+          title: 'Source SubTask 最大延迟 Top 15',
+          info: 'Source SubTask 最大延迟 Top 15'
         },
         {
-          title: '最小延时',
-          info: '衡量数据源的延迟情况，一段时间内的最小值'
+          title: '读TT延迟',
+          info: '读TT延迟'
         }
       ]
     };
 
   throughputPanel =
     {
-      header: 'Overview',
-      isActive: true,
+      header: 'Throughput',
+      isActive: false,
       charts: [
         {
-          title: 'FailOver rate',
-          info: '衡量当前Job发生failover的频率'
+          title: 'Task Input TPS',
+          info: 'Task Input TPS'
         },
         {
-          title: '最小延时',
-          info: '衡量数据源的延迟情况，一段时间内的最小值'
+          title: 'Task Output TPS',
+          info: 'Task Output TPS'
         }
       ]
     };
+
+
+  queuePanel =
+    {
+      header: 'Queue',
+      isActive: false,
+      charts: [
+        {
+          title: 'Input Queue Usage',
+          info: 'Input Queue Usage'
+        },
+        {
+          title: 'Output Queue Usage',
+          info: 'Output Queue Usage'
+        }
+      ]
+    };
+
+  tracingPanel =
+    {
+      header: 'Tracing',
+      isActive: false,
+      charts: [
+        {
+          title: 'Time Used In Processing Per Second',
+          info: 'Time Used In Processing Per Second'
+        },
+        {
+          title: 'Time Used In Waiting Oputput Per Second',
+          info: 'Time Used In Waiting Oputput Per Second'
+        },
+        {
+          title: 'TaskLatency Histogram Mean',
+          info: 'TaskLatency Histogram Mean'
+        },
+        {
+          title: 'WaitOutput Histogram Mean',
+          info: 'WaitOutput Histogram Mean'
+        },
+        {
+          title: 'WaitInput Histogram Mean',
+          info: 'WaitInput Histogram Mean'
+        },
+        {
+          // TODO: echarts 分页
+          title: 'PartitionLatency Mean',
+          info: 'PartitionLatency Mean'
+        }
+      ]
+    };
+
+
+  processPanel =
+    {
+      header: 'Process',
+      isActive: false,
+      charts: [
+        {
+          title: 'Process MEM Rss',
+          info: 'Process MEM Rss'
+        },
+        {
+          title: 'CPU Usage',
+          info: 'CPU Usage'
+        }
+      ]
+    };
+
+  jvmPanel =
+    {
+      header: 'JVM',
+      isActive: false,
+      charts: [
+        {
+          title: 'Memory Heap Used',
+          info: 'Memory Heap Used'
+        },
+        {
+          title: 'Memory NonHeap Used',
+          info: 'Memory NonHeap Used'
+        },
+        {
+          title: 'Threads Count',
+          info: 'Threads Count'
+        },
+        {
+          title: 'GC(CMS)',
+          info: 'GC(CMS)'
+        }
+      ]
+    };
+
+
+
 
   constructor() {
   }
