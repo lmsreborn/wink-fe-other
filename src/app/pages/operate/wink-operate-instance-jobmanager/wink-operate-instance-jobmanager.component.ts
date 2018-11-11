df17b65d82756c7494d78ab648c2d4fb55880112
Victor <lmslovecs@gmail.com>
2018-11-11 19:53:37 +0800

    feat(root): add new component in operate

--- src/app/pages/operate/wink-operate-instance-jobmanager/wink-operate-instance-jobmanager.component.ts
+++ src/app/pages/operate/wink-operate-instance-jobmanager/wink-operate-instance-jobmanager.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-wink-operate-instance-jobmanager',
   templateUrl: './wink-operate-instance-jobmanager.component.html',
-  styleUrls: ['./wink-operate-instance-jobmanager.component.css']
+  styleUrls: ['./wink-operate-instance-jobmanager.component.less']
 })
 export class WinkOperateInstanceJobmanagerComponent implements OnInit {
 
